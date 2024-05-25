bc7bdb590d3f3701296d4ec11c19b36ca904363f
Soham Bose <sohambose80@gmail.com>
2024-05-25 17:48:21 +0530

    added https frontend

--- admin/src/components/navbar/Navbar.jsx
+++ admin/src/components/navbar/Navbar.jsx
@@ -6,7 +6,7 @@ import navProfile from "../../assets/user image.gif"
 const Navbar = () => {
     return(
         <div className="navbar">
-            <img src={navlogo} alt="" className="nav-logo"/>
+           <a href="https://shoppiverse.vercel.app"><img src={navlogo} alt="" className="nav-logo"/></a> 
             <img src={navProfile} alt="" className="nav-profile"/>
 
         </div>
